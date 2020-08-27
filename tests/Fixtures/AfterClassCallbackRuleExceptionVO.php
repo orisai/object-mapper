86e9c80329c663b785b499630b620e338b9739f0
Marek Bartoš <bartos.developer152@gmail.com>
2020-08-27 11:40:29 +0200

    Coding style: import annotations one by one

--- tests/Fixtures/AfterClassCallbackRuleExceptionVO.php
+++ tests/Fixtures/AfterClassCallbackRuleExceptionVO.php
@@ -2,19 +2,19 @@
 
 namespace Tests\Orisai\ObjectMapper\Fixtures;
 
-use Orisai\ObjectMapper\Annotation\Callback;
-use Orisai\ObjectMapper\Annotation\Expect;
+use Orisai\ObjectMapper\Annotation\Callback\After;
+use Orisai\ObjectMapper\Annotation\Expect\StringValue;
 use Orisai\ObjectMapper\Callbacks\CallbackRuntime;
 use Orisai\ObjectMapper\Exception\ValueDoesNotMatch;
 use Orisai\ObjectMapper\ValueObject;
 
 /**
- * @Callback\After(method="after", runtime=CallbackRuntime::ALWAYS)
+ * @After(method="after", runtime=CallbackRuntime::ALWAYS)
  */
 final class AfterClassCallbackRuleExceptionVO extends ValueObject
 {
 
-	/** @Expect\StringValue() */
+	/** @StringValue() */
 	public string $string;
 
 	/**
