21ebc19f9ad69a040df4b94f5d43c8dbecf8f822
Mr Ceperka <johnnychudomsky@gmail.com>
2021-07-31 22:26:22 +0200

    PHPBench performace testing

---
+++ tests/Benchmark/ArrayOfBench.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Orisai\ObjectMapper\Benchmark;
+
+use Generator;
+use Orisai\ObjectMapper\Exception\InvalidData;
+use Tests\Orisai\ObjectMapper\Doubles\ArrayOfIntVO;
+use Tests\Orisai\ObjectMapper\Doubles\ArrayOfStringVO;
+use Tests\Orisai\ObjectMapper\Toolkit\ProcessingTestCase;
+use function array_fill;
+
+final class ArrayOfBench extends ProcessingTestCase
+{
+
+	/**
+	 * @Iterations(3)
+	 * @ParamProviders("provideArrayOfStrings")
+	 * @param array<string> $items
+	 * @throws InvalidData
+	 */
+	public function benchArrayOfString(array $items): void
+	{
+		$this->setUp();
+		$data = [
+			'items' => $items,
+		];
+
+		$this->processor->process($data, ArrayOfStringVO::class);
+	}
+
+	/**
+	 * @Iterations(3)
+	 * @ParamProviders("provideArrayOfInts")
+	 * @param array<int> $items
+	 * @throws InvalidData
+	 */
+	public function benchArrayOfInts(array $items): void
+	{
+		$this->setUp();
+		$data = [
+			'items' => $items,
+		];
+
+		$this->processor->process($data, ArrayOfIntVO::class);
+	}
+
+	public function provideArrayOfStrings(): Generator
+	{
+		yield '10k' => array_fill(0, 10_000, 'string');
+		yield '100k' => array_fill(0, 100_000, 'string');
+	}
+
+	public function provideArrayOfInts(): Generator
+	{
+		yield '10k' => array_fill(0, 10_000, 42);
+		yield '100k' => array_fill(0, 100_000, 42);
+	}
+
+}
---
+++ tests/Doubles/ArrayOfIntVO.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Orisai\ObjectMapper\Doubles;
+
+use Orisai\ObjectMapper\Annotation\Expect\ArrayOf;
+use Orisai\ObjectMapper\Annotation\Expect\IntValue;
+use Orisai\ObjectMapper\ValueObject;
+
+final class ArrayOfIntVO extends ValueObject
+{
+
+	/**
+	 * @var array<int>
+	 * @ArrayOf(
+	 *     @IntValue()
+	 * )
+	 */
+	public array $items;
+
+}
