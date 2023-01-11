2beb7ee4e6869933e5011b2e5170864ac113f26b
Marek Bartoš <bartos.developer152@gmail.com>
2023-01-11 23:18:33 +0100

    ClassModificationChecker: simplify, check whole hierarchy

--- src/Meta/ClassModificationsChecker.php
+++ src/Meta/ClassModificationsChecker.php
@@ -3,14 +3,8 @@
 namespace Orisai\ObjectMapper\Meta;
 
 use ReflectionClass;
-use function array_keys;
 use function array_merge;
-use function array_unique;
 use function assert;
-use function class_implements;
-use function class_parents;
-use function class_uses;
-use function trait_exists;
 
 final class ClassModificationsChecker
 {
@@ -21,75 +15,39 @@ final class ClassModificationsChecker
 	 */
 	public static function getSourceFiles(string $class): array
 	{
-		$files = [];
-		foreach (self::getAllTypes($class) as $type) {
-			$fileName = (new ReflectionClass($type))->getFileName();
-			if ($fileName === false) { // is internal
-				continue;
-			}
-
-			$files[] = $fileName;
-		}
-
-		return $files;
+		return self::get(new ReflectionClass($class));
 	}
 
 	/**
-	 * @param class-string $class
-	 * @return array<class-string>
+	 * @param ReflectionClass<object> $class
+	 * @return list<string>
 	 */
-	private static function getAllTypes(string $class): array
+	private static function get(ReflectionClass $class): array
 	{
-		$parents = class_parents($class);
-		assert($parents !== false);
-
-		$implements = class_implements($class);
-		assert($implements !== false);
-
-		return array_keys(
-			[$class => null]
-			+ $parents
-			+ $implements
-			+ self::getTraits($class),
-		);
-	}
-
-	/**
-	 * @param class-string $class
-	 * @return array<class-string>
-	 */
-	private static function getTraits(string $class): array
-	{
-		$traitsByClass = [];
-		$traitsByClass[] = self::getClassUsesRecursively($class);
-
-		$parents = class_parents($class);
-		assert($parents !== false);
-		foreach ($parents as $parent) {
-			$traitsByClass[] = self::getClassUsesRecursively($parent);
+		if ($class->isInternal()) {
+			return [];
 		}
 
-		return array_unique(array_merge(...$traitsByClass));
-	}
+		$filesByReflector = [];
 
-	/**
-	 * @param class-string $class
-	 * @return array<class-string>
-	 */
-	private static function getClassUsesRecursively(string $class): array
-	{
-		$traits = class_uses($class);
-		assert($traits !== false);
-
-		$traitsByTrait = [];
-		$traitsByTrait[] = $traits;
-
-		foreach ($traits as $trait) {
-			assert(trait_exists($trait));
-			$traitsByTrait[] = self::getClassUsesRecursively($trait);
+		$parent = $class->getParentClass();
+		if ($parent !== false) {
+			$filesByReflector[] = self::get($parent);
 		}
 
-		return array_merge(...$traitsByTrait);
+		foreach ($class->getInterfaces() as $interface) {
+			$filesByReflector[] = self::get($interface);
+		}
+
+		foreach ($class->getTraits() as $trait) {
+			$filesByReflector[] = self::get($trait);
+		}
+
+		$file = $class->getFileName();
+		assert($file !== false); // Only internal should be false
+		$filesByReflector[][] = $file;
+
+		return array_merge(...$filesByReflector);
 	}
 
 }
