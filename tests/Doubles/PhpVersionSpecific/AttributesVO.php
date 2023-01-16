40c9311845512c8d772e0593a70c6f2b42b9e5ed
Marek Bartoš <bartos.developer152@gmail.com>
2023-01-16 02:20:55 +0100

    Move some test doubles to PhpVersionSpecific

--- tests/Doubles/PhpVersionSpecific/AttributesVO.php
+++ tests/Doubles/PhpVersionSpecific/AttributesVO.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace Tests\Orisai\ObjectMapper\Doubles;
+namespace Tests\Orisai\ObjectMapper\Doubles\PhpVersionSpecific;
 
 use Orisai\ObjectMapper\Attributes\Expect\StringValue;
 use Orisai\ObjectMapper\MappedObject;
