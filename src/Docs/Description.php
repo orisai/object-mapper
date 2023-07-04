849323b0859664dfa856bc19e29c096c32e9fd9e
Marek Bartoš <bartos.developer152@gmail.com>
2023-07-04 20:14:57 +0200

    Replace AnnotationFilter with the one from orisai/reflection-meta

--- src/Docs/Description.php
+++ src/Docs/Description.php
@@ -5,7 +5,7 @@ namespace Orisai\ObjectMapper\Docs;
 use Attribute;
 use Doctrine\Common\Annotations\Annotation\NamedArgumentConstructor;
 use Doctrine\Common\Annotations\Annotation\Target;
-use Orisai\ObjectMapper\Meta\Source\AnnotationFilter;
+use Orisai\ReflectionMeta\Filter\AnnotationFilter;
 
 /**
  * @Annotation
